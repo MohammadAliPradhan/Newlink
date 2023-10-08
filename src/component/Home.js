32d353535f20d287f9949b164656ca20c2bdbff7
Mohammad Ali <aliiyusuf85@gmail.com>
2023-10-09 05:27:00 +0530

    updated

---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../cssfolder/Home.css"
+export default function(){
+    
+    return(
+        <>
+            <div className="parentDiv">
+                <div className="child1">
+                    <h1>Select Your New Perfect Style</h1>
+                </div>
+                <div className="child2"></div>
+                <div className="child3"></div>
+            </div>
+        </>
+        
+
+    )
+}
